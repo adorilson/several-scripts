c10e2336a74cbe641a5f5ad20686672b5d68efdd
Adorilson Bezerrra <adorilson@gmail.com>
2016-11-11 19:37:59 -0300

    Add script to Stackoverflow API

--- stackoverflow06.py
+++ stackoverflow06.py
@@ -3,6 +3,9 @@
 import requests, json
 from collections import Counter
 
+import urllib.parse
+import urllib3
+
 """
     Tarefa:
     Descobrir de onde os usuarios vem (país)
@@ -13,10 +16,24 @@ has_more = True
 page = 0
 while has_more:
     page = page + 1
-    key='8RIrpOz8rP2K8S4cIvZ*0g(('
-    target_url = 'http://api.stackexchange.com/2.2/users?order=desc&sort=reputation&site=pt.stackoverflow&key=%s&page=%s&pagesize=100'
-    response = requests.get(target_url % (key, page))
-    items = json.loads(response.content)
+    data = urllib.parse.urlencode({
+            'key':'8RIrpOz8rP2K8S4cIvZ*0g((',
+            'order':'desc',
+            'sort':'reputation',
+            'site':'pt.stackoverflow',
+            'page': page,
+            'pagesize': 100
+            })
+    target_url = 'http://api.stackexchange.com/2.2/users'
+    print(data)
+    full_url = target_url + '?' + str(data)
+    print(full_url)
+    response = urllib.request.urlopen(full_url)
+    #print(response.read()[:10])
+    
+    content = response.read()
+    print(content[:10])
+    items = json.loads(str(content, "utf-8"))
     languages_in_all_repo = list()
 
     has_more = items['has_more']
